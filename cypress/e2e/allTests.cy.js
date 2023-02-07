ae7d8cb11e0ece17b90f922bba1c852723a9490a
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-08 08:54:52 +1100

    Create test for invalid username

--- cypress/e2e/allTests.cy.js
+++ cypress/e2e/allTests.cy.js
@@ -7,11 +7,17 @@ const loginPage = new LoginPage();
 const productPage = new ProductPage();
 
 describe("Validate login functionality", () => {
+  // Visit page and verify login form loads before each test
   beforeEach(() => {
     cy.visit("");
     loginPage.loginFormExists();
   });
-
+  // Attempt login with invalid username
+  it("Unsuccessfully logs in invalid username", () => {
+    loginPage.login("invalid_user", "secret_sauce");
+    loginPage.errorMessageExists();
+  });
+  // Login with valid user
   it("Successfully logs in standard_user", () => {
     loginPage.login("standard_user", "secret_sauce");
     cy.get(".title").should("be.visible");
