11a3db3998f7f46fd98e4fe7896600a01e486f1d
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-02-07 18:20:36 +1100

    Create enterUsername() method

--- cypress/e2e/pages/LoginPage.js
+++ cypress/e2e/pages/LoginPage.js
@@ -2,4 +2,8 @@ export class LoginPage {
   usernameInput = '[data-test="username"]';
   passwordInput = '[data-test="password"]';
   loginButton = '[data-test="login-button"]';
+
+  enterUsername(username) {
+    cy.get(this.usernameInput).type(username);
+  }
 }
